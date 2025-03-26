d8646d40105b6aa31c78fea9e4f10db1f5449442
Ariyaman <ariyamandebnath.ad@gmail.com>
2025-03-26 18:31:05 +0530

    PostList for infinit scroll ui setup

--- Blog/src/components/PostList.tsx
+++ Blog/src/components/PostList.tsx
@@ -3,7 +3,10 @@ import PostListItems from "./PostListItems";
 
 const PostList: React.FC = () => {
   return (
-    <div className="flex flex-col gap-12 mb-8">
+    <div className="flex flex-col gap-12 mb-8 py-6">
+        <h1 className="text-3xl py-4 font-bold border-b-2">
+            Blogs
+        </h1>
       <PostListItems />
       <PostListItems />
       <PostListItems />
