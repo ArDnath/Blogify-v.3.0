0de045e6fc73f1b27ebbb9e3d1a9528df2c96e73
Ariyaman <ariyamandebnath.ad@gmail.com>
2025-04-28 23:40:32 +0530

    feat: add Dockerfile for multi-stage build and update backend configurations

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -2,7 +2,7 @@ import mongoose,{Mongoose} from 'mongoose';
 import { DB_NAME } from '../constants';
 
 
-export let dbInstance: Mongoose| undefined =undefined;
+
 
 const connectDB = async():Promise<void>=>{
 
@@ -10,15 +10,14 @@ const connectDB = async():Promise<void>=>{
         const connectionInstance = await mongoose.connect(
             `${process.env.MONGO_URI}/${DB_NAME}`,
         )
-        dbInstance =connectionInstance;
+    
         console.info(
             `\n☘️  MongoDB Connected! Db host: ${connectionInstance.connection.host}\n`
         );
         
     } catch (error) {
-        console.log("MongoDB connection error: ", error);
+        console.error("MongoDB connection error:", (error as Error).message);
         process.exit(1);
-        
     }
 }
 
